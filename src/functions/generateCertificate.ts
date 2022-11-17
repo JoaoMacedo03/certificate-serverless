4351182f824133981f8d6c645ed38ad8cd61a25f
Joao Macedo <joaovictormacedo91@gmail.com>
2022-11-17 19:26:43 -0300

    feat: creating pdf with handlebars

--- src/functions/generateCertificate.ts
+++ src/functions/generateCertificate.ts
@@ -1,4 +1,11 @@
 import { APIGatewayProxyHandler } from "aws-lambda"
+import { S3 } from 'aws-sdk'
+import { compile } from 'handlebars';
+import dayjs from 'dayjs'
+import { join } from 'path'
+import { readFileSync } from 'fs'
+import chromium from 'chrome-aws-lambda'
+
 import { document } from "../utils/dynamoDbClient";
 
 interface ICreateCertificate {
@@ -7,15 +14,23 @@ interface ICreateCertificate {
   grade: string;
 }
 
+interface ITemplate {
+  id: string;
+  name: string;
+  grade: string;
+  medal: string;
+  date: string;
+}
+
+const handleTemplate = async (data: ITemplate) => {
+  const filePath = join(process.cwd(), 'src', 'template', 'certificate.hbs')
+  const html = readFileSync(filePath, 'utf-8')
+
+  return compile(html)(data)
+}
+
 export const handler: APIGatewayProxyHandler = async (event) => {
   const { id, grade, name } = JSON.parse(event.body) as ICreateCertificate
-  
-  await document.put({
-    TableName: 'users_certificate',
-    Item: {
-      id, name, grade, created_at: new Date().getTime()
-    }
-  }).promise()
 
   const response = await document.query({
     TableName: 'users_certificate',
@@ -25,8 +40,61 @@ export const handler: APIGatewayProxyHandler = async (event) => {
     }
   }).promise()
 
+  const userAlreadyExist = response.Items[0]
+
+  if(!userAlreadyExist) {
+    await document.put({
+      TableName: 'users_certificate',
+      Item: {
+        id, name, grade, created_at: new Date().getTime()
+      }
+    }).promise()
+  }
+  
+  const medalPath = join(process.cwd(), 'src', 'template', 'selo.png')
+  const medal = readFileSync(medalPath, 'base64')
+
+  const data: ITemplate = {
+    name,
+    id,
+    grade,
+    date: dayjs().format('DD/MM/YYYY'),
+    medal
+  }
+
+  const content = await handleTemplate(data)
+
+  const browser = await chromium.puppeteer.launch({
+    args: chromium.args,
+    defaultViewport: chromium.defaultViewport,
+    executablePath: await chromium.executablePath
+  })
+
+  const page = await browser.newPage()
+
+  await page.setContent(content)
+
+  const pdf = await page.pdf({
+    format: 'a4',
+    landscape: true,
+    printBackground: true,
+    path: process.env.IS_OFFLINE ? './certificate.pdf': null
+  })
+
+  await browser.close()
+
+  const s3 = new S3()
+
+  await s3.putObject({
+    Bucket: 'certificate-ignite-joao-macedo',
+    Key: `${id}.pdf`,
+    Body: pdf,
+    ContentType: 'application/pdf'
+  }).promise()
+
   return {
     statusCode: 201,
-    body: JSON.stringify(response.Items[0])
+    message: 'Criado com sucesso',
+    body: JSON.stringify(response.Items[0]),
   }
 }
